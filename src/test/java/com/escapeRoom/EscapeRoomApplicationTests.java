fea3c16bc91899cbf0dc7cbe4d2208e93cab906e
WojciechKazura <wojciech.kazura@wp.pl>
2023-11-22 14:23:54 +0100

    Initial commit

---
+++ src/test/java/com/escapeRoom/EscapeRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.escapeRoom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscapeRoomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
